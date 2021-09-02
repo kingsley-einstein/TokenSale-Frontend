a3ef8a8a535e6d0164602ec5c172b66642e72aec
Kingsley Victor <javaprodigy56@gmail.com>
2021-09-02 21:36:54 +0100

    add presale contract abi

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,11 +2,13 @@ import React, { useState, useReducer } from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEquals } from "@fortawesome/free-solid-svg-icons";
+import Web3 from "web3";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import { initialState } from "../../state";
 import reducer from "../../state/reducer";
-import logo from "../../logo.svg";
+import logo from "../../xoximg.png";
+import PresaleABI from "../../assets/ABI.json";
 
 const NavFlex = styled.div`
   display: flex;
@@ -38,6 +40,14 @@ const SpanText = styled.span`
   padding: ${props => props.padding || 0};
 `;
 
+const AnchorLink = styled.a`
+  font-size: ${props => props.fontSize || "13px"};
+  font-weight: ${props => props.fontWeight || "normal"};
+  font-style: ${props => props.fontStyle || "normal"};
+  color: ${props => props.color || "black"};
+  padding: ${props => props.padding || 0};
+`;
+
 const DivInCenterFlex = styled.div`
   margin: ${props => props.margin || "0"};
 `;
@@ -46,40 +56,71 @@ const FaArrowsAltH = styled(FontAwesomeIcon)`
   font-size: ${props => props.fontSize || "12px"};
 `;
 
+const Image = styled.img`
+  width: ${props => props.width || "90px"};
+  height: ${props => props.height || "90px"};
+  border-radius: ${props => props.borderRadius || "50%"};
+`;
+
 const Home = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [bnbAmount, setBNBAmount] = useState(0);
+
+  const setProvider = provider =>
+    dispatch({
+      type: "WEB3_INJECTED",
+      payload: new Web3(provider)
+    });
+
+  const handleBNBInputChange = e => setBNBAmount(e.target.value);
+  const injectProvider = () => {
+    if (window.ethereum) {
+      setProvider(window.ethereum);
+      window.ethereum.send();
+    }
+  };
+
   return (
     <div>
       <NavFlex marginTop="2px" padding="2px">
         <div>
-          <img src={logo} width="90px" height="90px" alt="XOXCASH Logo" />
+          <Image
+            borderRadius="50%"
+            src={logo}
+            width="60px"
+            height="60px"
+            alt="XOXCASH Logo"
+          />
         </div>
         <Spacer />
         <CenterFlex>
-          <SpanText
+          <AnchorLink
+            href="https://discord.gg/qqFF3BtQDq"
             fontSize="19px"
             fontWeight="bold"
             color="#ffffff"
             padding="2px 6px 2px 6px"
           >
             Discord
-          </SpanText>
-          <SpanText
+          </AnchorLink>
+          <AnchorLink
+            href="https://t.me/xoxgroupchat"
             fontSize="19px"
             fontWeight="bold"
             color="#ffffff"
             padding="2px 6px 2px 6px"
           >
             Telegram
-          </SpanText>
-          <SpanText
+          </AnchorLink>
+          <AnchorLink
+            href="https://twitter.com/x0xcash"
             fontSize="19px"
             fontWeight="bold"
             color="#ffffff"
             padding="2px 6px 2px 6px"
           >
             Twitter
-          </SpanText>
+          </AnchorLink>
         </CenterFlex>
         <Spacer />
         <Button
@@ -109,7 +150,7 @@ const Home = () => {
             disabled
             padding="14px"
             border="0.7px solid #000000"
-            value="0.003 BNB"
+            value="0.0025 BNB"
           />
         </DivInCenterFlex>
       </CenterFlex>
@@ -120,6 +161,9 @@ const Home = () => {
             border="0.7px solid #000000"
             width="400px"
             placeholder="Enter BNB amount"
+            value={bnbAmount}
+            type="number"
+            onChange={handleBNBInputChange}
           />
         </DivInCenterFlex>
         <DivInCenterFlex margin="2px">
@@ -145,6 +189,17 @@ const Home = () => {
             padding="2px 6px 2px 6px"
           >
             Sale ends in:{" "}
+          </SpanText>
+          <SpanText
+            fontSize="34px"
+            fontWeight="bold"
+            color="#1642fa"
+            padding="1px 4px 1px 4px"
+          >
+            13
+          </SpanText>
+          <SpanText fontSize="15px" fontWeight="normal" color="#ffffff">
+            days
           </SpanText>
         </DivInCenterFlex>
       </CenterFlex>
